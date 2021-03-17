b38cba51a361f870d0268aacdbf1acac0261b5a2
TheBumpaster <TheBumpaster@users.noreply.github.com>
2021-03-17 15:59:17 +0000

    deploy: 50801cbff57b4b4d9a601a03ee18ca9eabf53d6c

---
+++ precache-manifest.4a56189e96fd106fa934dee18397e4f8.js
@@ -0,0 +1,170 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c7f491917978d354d5744be653b0479",
+    "url": "/erp_web/index.html"
+  },
+  {
+    "revision": "19585b563e795eb5d648",
+    "url": "/erp_web/static/css/2.89dee22b.chunk.css"
+  },
+  {
+    "revision": "61b45da115c7fd72eb3d",
+    "url": "/erp_web/static/css/main.4697d9cb.chunk.css"
+  },
+  {
+    "revision": "19585b563e795eb5d648",
+    "url": "/erp_web/static/js/2.be6dbde6.chunk.js"
+  },
+  {
+    "revision": "512fc405ebddfa8b215ddd04bc4e6e4e",
+    "url": "/erp_web/static/js/2.be6dbde6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a642a88462cc26bad11",
+    "url": "/erp_web/static/js/3.448cad56.chunk.js"
+  },
+  {
+    "revision": "19fd8456ca66af764b6d",
+    "url": "/erp_web/static/js/4.affada8d.chunk.js"
+  },
+  {
+    "revision": "f38afe5e9b3d319715b3",
+    "url": "/erp_web/static/js/5.1c84ff03.chunk.js"
+  },
+  {
+    "revision": "61b45da115c7fd72eb3d",
+    "url": "/erp_web/static/js/main.3a28cdea.chunk.js"
+  },
+  {
+    "revision": "029c6601ed090a6b444d09258de6b8ae",
+    "url": "/erp_web/static/js/main.3a28cdea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b50142d74302d7ab145",
+    "url": "/erp_web/static/js/runtime-main.a1bd0c1d.js"
+  },
+  {
+    "revision": "35d544eaaa4cf3c6355866280d53ba73",
+    "url": "/erp_web/static/media/Flaticon.35d544ea.eot"
+  },
+  {
+    "revision": "3e4331ee31764c999add7e0b048c4ba3",
+    "url": "/erp_web/static/media/Flaticon.3e4331ee.ttf"
+  },
+  {
+    "revision": "500d63cca8c534718b1d202b2076d218",
+    "url": "/erp_web/static/media/Flaticon.500d63cc.svg"
+  },
+  {
+    "revision": "5be3e43c13c3eb021d15e6682d098d4c",
+    "url": "/erp_web/static/media/Flaticon.5be3e43c.woff"
+  },
+  {
+    "revision": "29586ff0f963f4d1fdfc182822b8b27a",
+    "url": "/erp_web/static/media/Flaticon2.29586ff0.eot"
+  },
+  {
+    "revision": "4248a89254d2c6c396d85e378184c54f",
+    "url": "/erp_web/static/media/Flaticon2.4248a892.svg"
+  },
+  {
+    "revision": "b242ac810bd8cccaa03abc2128b7c3c3",
+    "url": "/erp_web/static/media/Flaticon2.b242ac81.woff"
+  },
+  {
+    "revision": "eafcbac04cdb0a39fe38a36ebd786290",
+    "url": "/erp_web/static/media/Flaticon2.eafcbac0.ttf"
+  },
+  {
+    "revision": "2a5ff834d12f21b421748941d6057d04",
+    "url": "/erp_web/static/media/Ki.2a5ff834.ttf"
+  },
+  {
+    "revision": "31f3692ca10d2f6d0bc467adba4533f7",
+    "url": "/erp_web/static/media/Ki.31f3692c.eot"
+  },
+  {
+    "revision": "34533d8034c943ae211ea85db35bfba9",
+    "url": "/erp_web/static/media/Ki.34533d80.svg"
+  },
+  {
+    "revision": "8f9a1a6cffbd1826644e12714ec106d0",
+    "url": "/erp_web/static/media/Ki.8f9a1a6c.woff"
+  },
+  {
+    "revision": "0cb5a5c0d251c109458c85c6afeffbaa",
+    "url": "/erp_web/static/media/fa-brands-400.0cb5a5c0.svg"
+  },
+  {
+    "revision": "13685372945d816a2b474fc082fd9aaa",
+    "url": "/erp_web/static/media/fa-brands-400.13685372.ttf"
+  },
+  {
+    "revision": "a06da7f0950f9dd366fc9db9d56d618a",
+    "url": "/erp_web/static/media/fa-brands-400.a06da7f0.woff2"
+  },
+  {
+    "revision": "c1868c9545d2de1cf8488f1dadd8c9d0",
+    "url": "/erp_web/static/media/fa-brands-400.c1868c95.eot"
+  },
+  {
+    "revision": "ec3cfddedb8bebd2d7a3fdf511f7c1cc",
+    "url": "/erp_web/static/media/fa-brands-400.ec3cfdde.woff"
+  },
+  {
+    "revision": "261d666b0147c6c5cda07265f98b8f8c",
+    "url": "/erp_web/static/media/fa-regular-400.261d666b.eot"
+  },
+  {
+    "revision": "89ffa3aba80d30ee0a9371b25c968bbb",
+    "url": "/erp_web/static/media/fa-regular-400.89ffa3ab.svg"
+  },
+  {
+    "revision": "c20b5b7362d8d7bb7eddf94344ace33e",
+    "url": "/erp_web/static/media/fa-regular-400.c20b5b73.woff2"
+  },
+  {
+    "revision": "db78b9359171f24936b16d84f63af378",
+    "url": "/erp_web/static/media/fa-regular-400.db78b935.ttf"
+  },
+  {
+    "revision": "f89ea91ecd1ca2db7e09baa2c4b156d1",
+    "url": "/erp_web/static/media/fa-regular-400.f89ea91e.woff"
+  },
+  {
+    "revision": "1ab236ed440ee51810c56bd16628aef0",
+    "url": "/erp_web/static/media/fa-solid-900.1ab236ed.ttf"
+  },
+  {
+    "revision": "a0369ea57eb6d3843d6474c035111f29",
+    "url": "/erp_web/static/media/fa-solid-900.a0369ea5.eot"
+  },
+  {
+    "revision": "b15db15f746f29ffa02638cb455b8ec0",
+    "url": "/erp_web/static/media/fa-solid-900.b15db15f.woff2"
+  },
+  {
+    "revision": "bea989e82b07e9687c26fc58a4805021",
+    "url": "/erp_web/static/media/fa-solid-900.bea989e8.woff"
+  },
+  {
+    "revision": "ec763292e583294612f124c0b0def500",
+    "url": "/erp_web/static/media/fa-solid-900.ec763292.svg"
+  },
+  {
+    "revision": "60e5857089e98edd838074c264d6c951",
+    "url": "/erp_web/static/media/socicon.60e58570.eot"
+  },
+  {
+    "revision": "944f06f5f65ef84a3a36e6c1c2d4b7ad",
+    "url": "/erp_web/static/media/socicon.944f06f5.woff"
+  },
+  {
+    "revision": "9a64ef938f9e55a70a4defc6ac9790bf",
+    "url": "/erp_web/static/media/socicon.9a64ef93.ttf"
+  },
+  {
+    "revision": "a35b65744f557fab5424e99bb6d4e980",
+    "url": "/erp_web/static/media/socicon.a35b6574.svg"
+  }
+]);
